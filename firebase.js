28517821f9fb76943b005033576e3fec7310d28a
erika2knd <erikakondratjeva@gmail.com>
2025-07-28 15:33:04 +0300

    new changes

--- firebase.js
+++ firebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZOpT9LdAxCrGZl4GybkAg79-oHCyrylI",
-  authDomain: "nft-marketplace-95ca0.firebaseapp.com",
-  projectId: "nft-marketplace-95ca0",
-  storageBucket: "nft-marketplace-95ca0.firebasestorage.app",
-  messagingSenderId: "286910343291",
-  appId: "1:286910343291:web:b374c7da9cd74c871a9ade"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export default app;
